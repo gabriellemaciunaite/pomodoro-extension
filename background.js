522b3aa7f7f6bd5448258c8d76b3f3b6cc1ddb4f
gabriellemaciunaite <gabrielle.maciunaite@warwick.ac.uk>
2026-07-31 23:57:53 +0100

    reduce code redundancy in background.js

--- background.js
+++ background.js
@@ -1,32 +1,17 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'START_TIMER') {
-        startTimer((targetTime) => sendResponse({ success: true, targetTime }));
-        return true; 
+        const targetTime = Date.now() + 1 * 60 * 1000;
+        chrome.storage.local.set({ isRunning: true, targetTime: targetTime });
+        chrome.alarms.create('pomodoroAlarm', { delayInMinutes: 1 });
     } else if (message.action === 'STOP_TIMER') {
-        stopTimer(() => sendResponse({ success: true }));
-        return true;
+        chrome.alarms.clear('pomodoroAlarm');
+        chrome.storage.local.set({ isRunning: false, targetTime: null });
     }
 });
 
-function startTimer(callback) {
-    const targetTime = Date.now() + 1 * 60 * 1000;
-    chrome.storage.local.set({ isRunning: true, targetTime: targetTime }, () => {
-        chrome.alarms.create('pomodoroAlarm', { delayInMinutes: 1 });
-        if (callback) callback(targetTime);
-    });
-}
-
-function stopTimer(callback) {
-    chrome.alarms.clear('pomodoroAlarm', () => {
-        chrome.storage.local.set({ isRunning: false, targetTime: null }, () => {
-            if (callback) callback();
-        });
-    });
-}
-
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === 'pomodoroAlarm') {
-        stopTimer();
+        chrome.storage.local.set({ isRunning: false, targetTime: null });
         chrome.notifications.create({
             type: 'basic',
             iconUrl: 'icon.png',
